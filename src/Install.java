4bdf97eaecbec83a2a41046d5d24d96a44dca952
Kelvin <kelvinc43@nycstudents.net>
2022-03-29 16:10:21 -0400

    (Finished?) all classes.

--- src/Install.java
+++ src/Install.java
@@ -5,14 +5,12 @@ public class Install {
     public void installHardDrive(String drive) {
         for (HardDrive i : HardDrive.getDriveList()) {
             if (drive.substring(0, drive.indexOf(" ")).equals(i.getName())) {
-                System.out.println("That drive name already exists!");
+                System.out.println("That drive name already exists!\n");
                 return;
             }
         }
         HardDrive newDrive = new HardDrive(drive.substring(0, drive.indexOf(" ")), Integer.parseInt(drive.substring(drive.indexOf(" ") + 1, drive.length() - 1)));
-
-        System.out.println("Drive " + newDrive.getName() + " Installed!");
-        System.out.println(newDrive.getSize());
+        System.out.println("Drive " + newDrive.getName() + " Installed!\n");
     }
 
     public String getDrives() {
@@ -20,14 +18,14 @@ public class Install {
         for (HardDrive i : HardDrive.getDriveList()) {
             list += i.getName() + " [" + i.getSize() + "G]\n";
         }
-        if (list.length() == 0) return "No drives";
+        if (list.length() == 0) return "No drives!\n";
         return list;
     }
 
     public String installPhysicalVolume(String drive) {
         for (PhysicalVolume i : PhysicalVolume.getVolumes()) {
             if (drive.substring(0, drive.indexOf(" ")).equals(i.getName())) {
-                return "That physical volume already exists!";
+                return "That physical volume already exists!\n";
             }
         }
         String name = drive.substring(0, drive.indexOf(" "));
@@ -36,12 +34,11 @@ public class Install {
         for (HardDrive i : HardDrive.getDriveList()) {
             if (drive.equals(i.getName())) d = i;
         }
-        if (d == null) return "No drive found named " + drive;
-        if (d.getVolume() != null) return "Drive " + drive + " already has a physical volume!";
-
+        if (d == null) return "No drive found named " + drive + "\n";
+        if (d.getVolume() != null) return "Drive " + drive + " already has a physical volume!\n";
         PhysicalVolume newPV = new PhysicalVolume(name, d);
         d.setVolume(newPV);
-        return name + " created!";
+        return name + " created!\n";
     }
 
     public String getPhysicalVolumes() {
@@ -52,14 +49,14 @@ public class Install {
             else temp += "[" + i.getGroup().getName() + "] [" + i.getUUID() + "]";
             list += temp + "\n";
         }
-        if (list.length() == 0) return "No Physical Volumes";
+        if (list.length() == 0) return "No Physical Volumes\n";
         return list;
     }
 
     public String installVolumeGroup(String drive) {
         for (VolumeGroup i : VolumeGroup.getGroups()) {
             if (drive.substring(0, drive.indexOf(" ")).equals(i.getName())) {
-                return "That volume group already exists!";
+                return "That volume group already exists!\n";
             }
         }
         String name = drive.substring(0, drive.indexOf(" "));
@@ -68,11 +65,11 @@ public class Install {
         for (PhysicalVolume i : PhysicalVolume.getVolumes()) {
             if (drive.equals(i.getName())) d = i;
         }
-        if (d == null) return "No volume found named " + drive;
-        if (d.getGroup() != null) return "Drive " + drive + " already has a physical volume!";
+        if (d == null) return "No volume found named " + drive + "\n";
+        if (d.getGroup() != null) return "Drive " + drive + " already has a physical volume!\n";
 
         VolumeGroup newVG = new VolumeGroup(name, d);
-        return name + " created!";
+        return name + " created!\n";
     }
     public String getVolumeGroups() {
         String list = "";
@@ -89,7 +86,7 @@ public class Install {
             }
             list += "] [" + i.getUUID() + "]\n";
         }
-        if (list.length() == 0) return "No Volume Groups";
+        if (list.length() == 0) return "No Volume Groups\n";
         return list;
     }
     public String extendVolumeGroup(String drive) {
@@ -100,7 +97,7 @@ public class Install {
             }
         }
         if (group == null) {
-            return drive + " does not exist.";
+            return drive + " does not exist.\n";
         }
         String name = drive.substring(0, drive.indexOf(" "));
         drive = drive.substring(drive.indexOf(" ") + 1);
@@ -109,13 +106,13 @@ public class Install {
             if (name.equals(i.getName())) volume = i;
         }
         if (volume.getGroup() != null) {
-            return name + " is already part of a volume group.";
+            return name + " is already part of a volume group.\n";
         }
         if (volume == null) {
-            return name + " is not an existing physical volume";
+            return name + " is not an existing physical volume.\n";
         }
         group.addPV(volume);
-        return name + "extended!";
+        return name + "extended!\n";
     }
     public String installLogicalVolume(String drive) {
         String name = drive.substring(0, drive.indexOf(" "));
@@ -124,26 +121,26 @@ public class Install {
         drive = drive.substring(0, drive.indexOf(" ") + 1);
         for (LogicalVolume i : LogicalVolume.getLogicalList()) {
             if (name.equals(i.getName())) {
-                return "That Logical Volume already exists!";
+                return "That Logical Volume already exists!\n";
             }
         }
         VolumeGroup d = null;
         for (VolumeGroup i : VolumeGroup.getGroups()) {
             if (VG.equals(i.getName())) d = i;
         }
-        if (d == null) return "No volume group found named " + drive;
+        if (d == null) return "No volume group found named " + drive + "\n";
         int i = Integer.parseInt(name.substring(0, name.length() - 1));
-        if (i > d.getStorage()) return "There is no storage remaining!";
+        if (i > d.getStorage()) return "There is no storage remaining!\n";
         LogicalVolume v = new LogicalVolume(name, i, d);
         d.addLV(v);
-        return name + " created!";
+        return name + " created!\n";
     }
     public String getLogicalVolume() {
         String list = "";
         for (LogicalVolume i : LogicalVolume.getLogicalList()) {
             list += i.getName() + ": [" + i.getSize() + "G] [" + i.getGroup().getName() + "] [" + i.getUUID() + "]\n";
         }
-        if (list.length() == 0) return "No Logical Volumes";
+        if (list.length() == 0) return "No Logical Volumes!\n";
         return list;
     }
 }
